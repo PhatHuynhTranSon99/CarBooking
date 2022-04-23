a7c2fba20c61088c76ec30c95be531548880b96e
phat <phat.huynhtranso99@gmail.com>
2022-04-23 10:36:46 +0700

    Add find available cars endpoint

---
+++ src/main/java/com/transonphat/carbooking/controllers/BookingController.java
@@ -0,0 +1,29 @@
+package com.transonphat.carbooking.controllers;
+
+import com.transonphat.carbooking.domain.Car;
+import com.transonphat.carbooking.pagination.PaginationResult;
+import com.transonphat.carbooking.services.BookingService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+
+@RestController
+public class BookingController {
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/booking/cars")
+    public PaginationResult<Car> findAvailableCars(@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime start,
+                                                   @RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime end,
+                                                   @RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "3") int size) {
+        System.out.println("YESSSSSSSSSSSSS");
+        return bookingService.findAvailableCars(start, end, page, size);
+    }
+}
--- src/main/java/com/transonphat/carbooking/services/BookingService.java
+++ src/main/java/com/transonphat/carbooking/services/BookingService.java
@@ -1,5 +1,37 @@
 package com.transonphat.carbooking.services;
 
-public class BookingService {
+import com.transonphat.carbooking.dao.DAO;
+import com.transonphat.carbooking.dao.SearchableDAO;
+import com.transonphat.carbooking.domain.Car;
+import com.transonphat.carbooking.domain.Invoice;
+import com.transonphat.carbooking.pagination.PaginationResult;
+import com.transonphat.carbooking.search.car.CarFreeCriterion;
+import org.springframework.stereotype.Service;
 
+import java.time.ZonedDateTime;
+
+@Service
+public class BookingService {
+    private DAO<Invoice> invoiceDao;
+    private SearchableDAO<Car> carSearchableDAO;
+    private SearchableDAO<Invoice> invoiceSearchableDAO;
+
+    public BookingService(DAO<Invoice> invoiceDao,
+                          SearchableDAO<Car> carSearchableDAO,
+                          SearchableDAO<Invoice> invoiceSearchableDAO) {
+        this.invoiceDao = invoiceDao;
+        this.carSearchableDAO = carSearchableDAO;
+        this.invoiceSearchableDAO = invoiceSearchableDAO;
+    }
+
+    public PaginationResult<Car> findAvailableCars(ZonedDateTime startTime,
+                                                   ZonedDateTime endTime,
+                                                   int currentPage,
+                                                   int pageSize) {
+        return this.carSearchableDAO.search(
+                new CarFreeCriterion(startTime, endTime),
+                currentPage,
+                pageSize
+        );
+    }
 }
