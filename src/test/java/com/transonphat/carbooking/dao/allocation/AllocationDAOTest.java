59d0301d7625ca13d74a98f45e059e6ab14b9116
phat <phat.huynhtranso99@gmail.com>
2022-05-02 11:39:15 +0700

    Add Allocation DAO Test

---
+++ src/test/java/com/transonphat/carbooking/dao/allocation/AllocationDAOTest.java
@@ -0,0 +1,9 @@
+package com.transonphat.carbooking.dao.allocation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(profiles = {"test"})
+public class AllocationDAOTest {
+}
