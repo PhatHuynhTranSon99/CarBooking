9e626932b02ff318a0b7256af1ae50b43910c1a8
phat <phat.huynhtranso99@gmail.com>
2022-05-02 11:34:02 +0700

    Add driver service test

--- src/test/java/com/transonphat/carbooking/services/DriverServiceTest.java
+++ src/test/java/com/transonphat/carbooking/services/DriverServiceTest.java
@@ -1,4 +1,52 @@
 package com.transonphat.carbooking.services;
 
+import com.transonphat.carbooking.dao.DAO;
+import com.transonphat.carbooking.dao.SearchableDAO;
+import com.transonphat.carbooking.domain.Driver;
+import com.transonphat.carbooking.search.SearchCriterion;
+import com.transonphat.carbooking.search.driver.DriverNameCriterion;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {DriverService.class})
+@ActiveProfiles(profiles = {"test"})
 public class DriverServiceTest {
+    @Autowired
+    private DriverService driverService;
+
+    @MockBean
+    private DAO<Driver> driverDAO;
+
+    @MockBean
+    private SearchableDAO<Driver> driverSearchableDAO;
+
+    @Test
+    public void saveDriverShouldCallDAOMethod() {
+        Driver newDriver = new Driver();
+        driverService.saveDriver(newDriver);
+        Mockito.verify(driverDAO).save(newDriver);
+    }
+
+    @Test
+    public void deleteDriverShouldCallDAOMethod() {
+        driverService.deleteDriver(1L);
+        Mockito.verify(driverDAO).delete(1L);
+    }
+
+    @Test
+    public void getDriverByIdShouldCallDAOMethod() {
+        driverService.getDriverById(100L);
+        Mockito.verify(driverDAO).getOne(100L);
+    }
+
+    @Test
+    public void searchDriverShouldCallDAOMethod() {
+        SearchCriterion<Driver> driverSearchCriterion = new DriverNameCriterion("");
+        driverService.searchDriver(driverSearchCriterion, 0, 10);
+        Mockito.verify(driverSearchableDAO).search(driverSearchCriterion, 0, 10);
+    }
 }
