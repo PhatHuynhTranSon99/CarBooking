235d98af6ec246f743c9448b1b76e6c11a69e525
phat <phat.huynhtranso99@gmail.com>
2022-05-02 13:58:04 +0700

    Add allocation service test

--- src/test/java/com/transonphat/carbooking/services/AllocationServiceTest.java
+++ src/test/java/com/transonphat/carbooking/services/AllocationServiceTest.java
@@ -1,4 +1,206 @@
 package com.transonphat.carbooking.services;
 
+import com.transonphat.carbooking.dao.DAO;
+import com.transonphat.carbooking.dao.allocation.AllocationDAO;
+import com.transonphat.carbooking.domain.Car;
+import com.transonphat.carbooking.domain.CarBuilder;
+import com.transonphat.carbooking.domain.Driver;
+import com.transonphat.carbooking.exceptions.CarAllocationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = {AllocationService.class})
+@ActiveProfiles(profiles = {"test"})
 public class AllocationServiceTest {
+    @Autowired
+    private AllocationService allocationService;
+
+    @MockBean
+    private DAO<Car> carDAO;
+
+    @MockBean
+    private DAO<Driver> driverDAO;
+
+    @MockBean
+    private AllocationDAO allocationDAO;
+
+    @Test
+    public void shouldAllocateCarSuccessfully() {
+        //Create mock driver and car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Toyota")
+                .setModel("Vias")
+                .setColor("Green")
+                .setConvertible(true)
+                .setIdentificationNumber("0180-989")
+                .setLicensePlate("G1-0172")
+                .setRating(4.5)
+                .setRate(10.8)
+                .build();
+
+        Driver driver = new Driver(
+                1L,
+                "Adam",
+                "Levine",
+                "0909111909",
+                4.9,
+                ZonedDateTime.now()
+        );
+
+        car.setDriver(null);
+        driver.setCar(null);
+
+        when(carDAO.getOne(1L)).thenReturn(car);
+        when(driverDAO.getOne(1L)).thenReturn(driver);
+
+        //Call allocate service
+        allocationService.allocateDriverToCar(1L, 1L);
+
+        //One for check car allocated, one for get one
+        verify(carDAO).getOne(1L);
+        verify(driverDAO).getOne(1L);
+        verify(allocationDAO).addCarToDriver(car, driver);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCarIsAllocated() {
+        //Create mock driver and car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Toyota")
+                .setModel("Vias")
+                .setColor("Green")
+                .setConvertible(true)
+                .setIdentificationNumber("0180-989")
+                .setLicensePlate("G1-0172")
+                .setRating(4.5)
+                .setRate(10.8)
+                .build();
+
+        Driver driver = new Driver(
+                1L,
+                "Adam",
+                "Levine",
+                "0909111909",
+                4.9,
+                ZonedDateTime.now()
+        );
+
+        car.setDriver(new Driver());
+        driver.setCar(null);
+
+        when(carDAO.getOne(1L)).thenReturn(car);
+        when(driverDAO.getOne(1L)).thenReturn(driver);
+
+        //Call allocate service
+        CarAllocationException exception = assertThrows(
+                CarAllocationException.class,
+                () -> {
+                    allocationService.allocateDriverToCar(1L, 1L);
+                }
+        );
+
+        assertEquals("Car is already allocated", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenDriverIsAllocated() {
+        //Create mock driver and car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Toyota")
+                .setModel("Vias")
+                .setColor("Green")
+                .setConvertible(true)
+                .setIdentificationNumber("0180-989")
+                .setLicensePlate("G1-0172")
+                .setRating(4.5)
+                .setRate(10.8)
+                .build();
+
+        Driver driver = new Driver(
+                1L,
+                "Adam",
+                "Levine",
+                "0909111909",
+                4.9,
+                ZonedDateTime.now()
+        );
+
+        car.setDriver(null);
+        driver.setCar(new Car());
+
+        when(carDAO.getOne(1L)).thenReturn(car);
+        when(driverDAO.getOne(1L)).thenReturn(driver);
+
+        //Call allocate service
+        CarAllocationException exception = assertThrows(
+                CarAllocationException.class,
+                () -> {
+                    allocationService.allocateDriverToCar(1L, 1L);
+                }
+        );
+
+        assertEquals("Driver is already allocated to a car", exception.getMessage());
+    }
+
+    @Test
+    public void shouldDeallocatedSuccessfully() {
+        //Create mock driver and car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Toyota")
+                .setModel("Vias")
+                .setColor("Green")
+                .setConvertible(true)
+                .setIdentificationNumber("0180-989")
+                .setLicensePlate("G1-0172")
+                .setRating(4.5)
+                .setRate(10.8)
+                .build();
+        car.setDriver(new Driver());
+
+        when(carDAO.getOne(1L)).thenReturn(car);
+
+        allocationService.removeDriverFromCar(1L);
+        verify(carDAO, atLeast(1)).getOne(1L);
+        verify(allocationDAO).removeCarFromDriver(car.getDriver());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCarIsNotAllocated() {
+        //Create mock driver and car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Toyota")
+                .setModel("Vias")
+                .setColor("Green")
+                .setConvertible(true)
+                .setIdentificationNumber("0180-989")
+                .setLicensePlate("G1-0172")
+                .setRating(4.5)
+                .setRate(10.8)
+                .build();
+        car.setDriver(null);
+
+        when(carDAO.getOne(1L)).thenReturn(car);
+
+        CarAllocationException exception = assertThrows(
+                CarAllocationException.class,
+                () -> {
+                    allocationService.removeDriverFromCar(1L);
+                }
+        );
+        assertEquals("Car is not allocated yet", exception.getMessage());
+    }
 }
