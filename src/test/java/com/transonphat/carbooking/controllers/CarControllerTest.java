0255dfc09c0821e31fa45266135617da0a159e26
phat <phat.huynhtranso99@gmail.com>
2022-05-03 15:24:13 +0700

    Add car controller test

--- src/test/java/com/transonphat/carbooking/controllers/CarControllerTest.java
+++ src/test/java/com/transonphat/carbooking/controllers/CarControllerTest.java
@@ -12,12 +12,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -130,22 +132,149 @@ public class CarControllerTest {
     }
 
     @Test
-    public void updateCarSuccessfully() {
+    public void updateCarSuccessfully() throws Exception {
+        //New attribute
+        String newVIN = "0111-022";
+        String newLicensePlate = "G5-5655";
+        String newColor = "Green";
+        String newMake = "MAZDA";
+        String newModel = "CSS";
+        Boolean newConvertible = false;
+        Double newRating = 3.0;
+        Double newRate = 9.0;
+
+        //Return car when find
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Mazda")
+                .setModel("CSV")
+                .setColor("Blue")
+                .setConvertible(false)
+                .setIdentificationNumber("0111-012")
+                .setLicensePlate("G1-5655")
+                .setRating(3.6)
+                .setRate(5.5)
+                .build();
+
+        Car newCar = new CarBuilder()
+                .setId(1L)
+                .setMake(newMake)
+                .setModel(newModel)
+                .setColor(newColor)
+                .setConvertible(newConvertible)
+                .setIdentificationNumber(newVIN)
+                .setLicensePlate(newLicensePlate)
+                .setRating(newRating)
+                .setRate(newRate)
+                .build();
+
+        when(carService.getCarById(1L)).thenReturn(car);
+        when(carService.saveCar(any())).thenReturn(newCar);
+
+        //Params
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("identification", newVIN);
+        requestParams.add("color", newColor);
+        requestParams.add("make", newMake);
+        requestParams.add("model", newModel);
+        requestParams.add("convertible", String.valueOf(newConvertible));
+        requestParams.add("rating", String.valueOf(newRating));
+        requestParams.add("rate", String.valueOf(newRate));
+        requestParams.add("licensePlate", newLicensePlate);
+
+
+        mockMvc.perform(
+                put("/cars/1").params(requestParams)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(newCar.getId()))
+                .andExpect(jsonPath("$.make").value(newCar.getMake()))
+                .andExpect(jsonPath("$.model").value(newCar.getModel()))
+                .andExpect(jsonPath("$.color").value(newCar.getColor()))
+                .andExpect(jsonPath("$.isConvertible").value(newCar.getIsConvertible()))
+                .andExpect(jsonPath("$.identificationNumber").value(newCar.getIdentificationNumber()))
+                .andExpect(jsonPath("$.licensePlate").value(newCar.getLicensePlate()))
+                .andExpect(jsonPath("$.rating").value(newCar.getRating()))
+                .andExpect(jsonPath("$.rate").value(newCar.getRate()))
+                .andDo(print());
 
     }
 
     @Test
-    public void updateCarThrowExceptionWhenNotFound() {
+    public void updateCarThrowExceptionWhenNotFound() throws Exception {
+        when(carService.getCarById(1L)).thenThrow(new CarNotFoundException("Car does not exist"));
 
+        //Update params
+        String newVIN = "0111-022";
+        String newLicensePlate = "G5-5655";
+        String newColor = "Green";
+        String newMake = "MAZDA";
+        String newModel = "CSS";
+        Boolean newConvertible = false;
+        Double newRating = 3.0;
+        Double newRate = 9.0;
+
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("identification", newVIN);
+        requestParams.add("color", newColor);
+        requestParams.add("make", newMake);
+        requestParams.add("model", newModel);
+        requestParams.add("convertible", String.valueOf(newConvertible));
+        requestParams.add("rating", String.valueOf(newRating));
+        requestParams.add("rate", String.valueOf(newRate));
+        requestParams.add("licensePlate", newLicensePlate);
+
+        mockMvc.perform(
+                put("/cars/1").params(requestParams)
+        )
+                .andExpect(status().isInternalServerError())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof CarNotFoundException));
     }
 
     @Test
-    public void deleteCarByIdSuccessfully() {
+    public void deleteCarByIdSuccessfully() throws Exception {
+        //When call delete -> Return a car
+        Car car = new CarBuilder()
+                .setId(1L)
+                .setMake("Mazda")
+                .setModel("CSV")
+                .setColor("Blue")
+                .setConvertible(false)
+                .setIdentificationNumber("0111-012")
+                .setLicensePlate("G1-5655")
+                .setRating(3.6)
+                .setRate(5.5)
+                .build();
+        when(carService.deleteCar(1L)).thenReturn(car);
 
+        //Call endpoint
+        mockMvc.perform(delete("/cars/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(car.getId()))
+                .andExpect(jsonPath("$.make").value(car.getMake()))
+                .andExpect(jsonPath("$.model").value(car.getModel()))
+                .andExpect(jsonPath("$.color").value(car.getColor()))
+                .andExpect(jsonPath("$.isConvertible").value(car.getIsConvertible()))
+                .andExpect(jsonPath("$.identificationNumber").value(car.getIdentificationNumber()))
+                .andExpect(jsonPath("$.licensePlate").value(car.getLicensePlate()))
+                .andExpect(jsonPath("$.rating").value(car.getRating()))
+                .andExpect(jsonPath("$.rate").value(car.getRate()));
+
+        verify(bookingService).deleteRelatedBookingsWithCar(1L);
     }
 
     @Test
-    public void getAllCarSuccessfully() {
+    public void getAllCarSuccessfully() throws Exception {
+        //Params
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("color", "Blue");
 
+        //Assert that search car is called
+        mockMvc.perform(
+                get("/cars")
+                        .params(requestParams)
+        );
+
+        verify(carService).searchCar(any(), anyInt(), anyInt());
     }
 }
