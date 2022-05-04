9dafd1b9f7f41cafa082a71904391e8be4a06c4e
phat <phat.huynhtranso99@gmail.com>
2022-05-04 08:39:52 +0700

    Change statistic controller to use service

--- src/test/java/com/transonphat/carbooking/controllers/StatisticControllerTest.java
+++ src/test/java/com/transonphat/carbooking/controllers/StatisticControllerTest.java
@@ -1,4 +1,15 @@
 package com.transonphat.carbooking.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(StatisticController.class)
 public class StatisticControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StatisticController statisticController;
 }
