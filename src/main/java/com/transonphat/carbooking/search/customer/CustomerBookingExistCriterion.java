34a3876861d492d99db930c9e7d804a446b9efd1
phat <phat.huynhtranso99@gmail.com>
2022-05-04 15:51:36 +0700

    Add customer booking search

---
+++ src/main/java/com/transonphat/carbooking/search/customer/CustomerBookingExistCriterion.java
@@ -0,0 +1,46 @@
+package com.transonphat.carbooking.search.customer;
+
+import com.transonphat.carbooking.domain.*;
+import com.transonphat.carbooking.search.SearchCriterion;
+
+import javax.persistence.criteria.*;
+import java.time.ZonedDateTime;
+
+public class CustomerBookingExistCriterion implements SearchCriterion<Customer> {
+    private long customerId;
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+
+    public CustomerBookingExistCriterion(long customerId, ZonedDateTime startTime, ZonedDateTime endTime) {
+        this.customerId = customerId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        Subquery<Long> customerIdQuery = query.subquery(Long.class);
+        Root<Booking> bookingRoot = customerIdQuery.from(Booking.class);
+
+        //Query booking
+        Predicate predicate = criteriaBuilder.and(
+                criteriaBuilder.lessThan(bookingRoot.get(Booking_.startTime), endTime),
+                criteriaBuilder.greaterThan(bookingRoot.get(Booking_.endTime), startTime),
+                criteriaBuilder.equal(
+                        bookingRoot.get(Booking_.invoice)
+                            .get(Invoice_.customer)
+                            .get(Customer_.id),
+                        customerId
+                )
+        );
+        customerIdQuery
+                .select(
+                        bookingRoot.get(Booking_.invoice)
+                                .get(Invoice_.customer)
+                                .get(Customer_.id)
+                )
+                .where(predicate);
+
+        return root.get(Car_.id).in(customerIdQuery);
+    }
+}
